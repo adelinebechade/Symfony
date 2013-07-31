85215139c97a3763f266673694ec58bba54e62ba
Adeline <becad@Adeline-PC.ProximIT.local>
2013-07-31 17:30:52 +0200

    Fin Partie 3.5 et début TP 3.6

--- src/Sdz/BlogBundle/Entity/Commentaire.php
+++ src/Sdz/BlogBundle/Entity/Commentaire.php
@@ -1,148 +1,126 @@
 <?php
-
+// src/Sdz/Blog/Bundle/Entity/Commentaire.php
+ 
 namespace Sdz\BlogBundle\Entity;
  
 use Doctrine\ORM\Mapping as ORM;
  
 /**
- * Sdz\BlogBundle\Entity\Commentaire
- *
- * @ORM\Table(name="commentaire")
+ * @ORM\Table()
  * @ORM\Entity(repositoryClass="Sdz\BlogBundle\Entity\CommentaireRepository")
  */
 class Commentaire
 {
-    /**
-     * @ORM\ManyToOne(targetEntity="Sdz\BlogBundle\Entity\Article", inversedBy="commentaires")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $article;
-
-    /**
-     * @var integer $id
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
-
-    /**
-     * @var string $auteur
-     *
-     * @ORM\Column(name="auteur", type="string", length=255)
-     */
-    private $auteur;
-
-    /**
-     * @var text $contenu
-     *
-     * @ORM\Column(name="contenu", type="text")
-     */
-    private $contenu;
-
-    /**
-     * @var datetime $date
-     *
-     * @ORM\Column(name="date", type="datetime")
-     */
-    private $date;
-
-    public function __construct()
-    {
-      $this->date = new \Datetime();
-    }
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-      return $this->id;
-    }
-
-    /**
-     * Set auteur
-     *
-     * @param string $auteur
-     */
-    public function setAuteur($auteur)
-    {
-      $this->auteur = $auteur;
-    }
-
-    /**
-     * Get auteur
-     *
-     * @return string
-     */
-    public function getAuteur()
-    {
+  /**
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
+ 
+  /**
+   * @ORM\Column(name="auteur", type="string", length=255)
+   */
+  private $auteur;
+ 
+  /**
+   * @ORM\Column(name="contenu", type="text")
+   */
+  private $contenu;
+ 
+  /**
+   * @ORM\Column(name="date", type="datetime")
+   */
+  private $date;
+ 
+  /**
+   * @ORM\ManyToOne(targetEntity="Sdz\BlogBundle\Entity\Article", inversedBy="commentaires")
+   * @ORM\JoinColumn(nullable=false)
+   */
+  private $article;
+ 
+  public function __construct()
+  {
+    $this->date = new \Datetime();
+  }
+ 
+  /**
+   * @return integer
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+ 
+  /**
+   * @param string $auteur
+   * @return Commentaire
+   */
+  public function setAuteur($auteur)
+  {
+    $this->auteur = $auteur;
+    return $this;
+  }
+ 
+  /**
+   * @return string
+   */
+  public function getAuteur()
+  {
     return $this->auteur;
-    }
-
-    /**
-     * Set contenu
-     *
-     * @param text $contenu
-     */
-    public function setContenu($contenu)
-    {
+  }
+ 
+  /**
+   * @param text $contenu
+   * @return Commentaire
+   */
+  public function setContenu($contenu)
+  {
     $this->contenu = $contenu;
-    }
-
-    /**
-     * Get contenu
-     *
-     * @return text
-     */
-    public function getContenu()
-    {
-      return $this->contenu;
-    }
-
-    /**
-     * Set date
-     *
-     * @param datetime $date
-     */
-    public function setDate(\Datetime $date)
-    {
-      $this->date = $date;
-    }
-
-    /**
-     * Get date
-     *
-     * @return datetime
-     */
-    public function getDate()
-    {
-      return $this->date;
-    }
-
-    /**
-     * Set article
-     *
-     * @param \Sdz\BlogBundle\Entity\Article $article
-     * @return Commentaire
-     */
-    public function setArticle(\Sdz\BlogBundle\Entity\Article $article)
-    {
-        $this->article = $article;
-
-        return $this;
-    }
-
-    /**
-     * Get article
-     *
-     * @return \Sdz\BlogBundle\Entity\Article 
-     */
-    public function getArticle()
-    {
-        return $this->article;
-    }
+    return $this;
+  }
+ 
+  /**
+   * @return text
+   */
+  public function getContenu()
+  {
+    return $this->contenu;
+  }
+ 
+  /**
+   * @param datetime $date
+   * @return Commentaire
+   */
+  public function setDate(\Datetime $date)
+  {
+    $this->date = $date;
+    return $this;
+  }
+ 
+  /**
+   * @return datetime
+   */
+  public function getDate()
+  {
+    return $this->date;
+  }
+ 
+  /**
+   * @param Sdz\BlogBundle\Entity\Article $article
+   * @return Commentaire
+   */
+  public function setArticle(\Sdz\BlogBundle\Entity\Article $article)
+  {
+    $this->article = $article;
+    return $this;
+  }
+ 
+  /**
+   * @return Sdz\BlogBundle\Entity\Article
+   */
+  public function getArticle()
+  {
+    return $this->article;
+  }
 }
--- src/Sdz/BlogBundle/Entity/Competence.php
+++ src/Sdz/BlogBundle/Entity/Competence.php
@@ -6,58 +6,51 @@ namespace Sdz\BlogBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
  
 /**
- * Sdz\BlogBundle\Entity\Competence
- *
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="Sdz\BlogBundle\Entity\CompetenceRepository")
  */
 class Competence
 {
-    /**
-      * @var integer $id
-      *
-      * @ORM\Column(name="id", type="integer")
-      * @ORM\Id
-      * @ORM\GeneratedValue(strategy="AUTO")
-      */
-    private $id;
-
-    /**
-      * @var string $nom
-      *
-      * @ORM\Column(name="nom", type="string", length=255)
-      */
-    private $nom;
-
-
-    /**
-      * Get id
-      *
-      * @return integer 
-      */
-    public function getId()
-    {
-      return $this->id;
-    }
-
-    /**
-      * Set nom
-      *
-      * @param string $nom
-      * @return Competence
-      */
-    public function setNom($nom)
-    {
-      $this->nom = $nom;
-    }
-
-    /**
-      * Get nom
-      *
-      * @return string 
-      */
-    public function getNom()
-    {
-      return $this->nom;
-    }
+  /**
+   * @var integer $id
+   *
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
+ 
+  /**
+   * @var string $nom
+   *
+   * @ORM\Column(name="nom", type="string", length=255)
+   */
+  private $nom;
+ 
+ 
+  /**
+   * @return integer 
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+ 
+  /**
+   * @param string $nom
+   * @return Competence
+   */
+  public function setNom($nom)
+  {
+    $this->nom = $nom;
+    return $this;
+  }
+ 
+  /**
+   * @return string 
+   */
+  public function getNom()
+  {
+    return $this->nom;
+  }
 }
