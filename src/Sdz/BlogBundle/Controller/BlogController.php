9ee07fb9bcd95d6a05cdf174b173e347a1cfa0ad
Adeline <becad@Adeline-PC.ProximIT.local>
2013-08-02 16:09:02 +0200

    Fin Partie 4.1

--- src/Sdz/BlogBundle/Controller/BlogController.php
+++ src/Sdz/BlogBundle/Controller/BlogController.php
@@ -48,23 +48,38 @@ class BlogController extends Controller
   public function ajouterAction()
   {
     $article = new Article;
-    $article->setPublication(false);
-
-    $form = $this->createForm(new ArticleType, $article);
-
-    $request = $this->get('request');
+ 
+    // On crée le formulaire grâce à l'ArticleType
+    $form = $this->createForm(new ArticleType(), $article);
+ 
+    // On récupère la requête
+    $request = $this->getRequest();
+ 
+    // On vérifie qu'elle est de type POST
     if ($request->getMethod() == 'POST') {
+      // On fait le lien Requête <-> Formulaire
       $form->bind($request);
-
+ 
+      // On vérifie que les valeurs entrées sont correctes
+      // (Nous verrons la validation des objets en détail dans le prochain chapitre)
       if ($form->isValid()) {
+        // On enregistre notre objet $article dans la base de données
         $em = $this->getDoctrine()->getManager();
         $em->persist($article);
         $em->flush();
-
-        return $this->redirect($this->generateUrl('sdzblog_accueil'));
+ 
+        // On définit un message flash
+        $this->get('session')->getFlashBag()->add('info', 'Article bien ajouté');
+ 
+        // On redirige vers la page de visualisation de l'article nouvellement créé
+        return $this->redirect($this->generateUrl('sdzblog_voir', array('id' => $article->getId())));
       }
     }
-
+ 
+    // À ce stade :
+    // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+    // - Soit la requête est de type POST, mais le formulaire n'est pas valide, donc on l'affiche de nouveau
+ 
     return $this->render('SdzBlogBundle:Blog:ajouter.html.twig', array(
       'form' => $form->createView(),
     ));
@@ -72,60 +87,61 @@ class BlogController extends Controller
  
   public function modifierAction(Article $article)
   {
-    // On utilise le ArticleEditType
+    // On utiliser le ArticleEditType
     $form = $this->createForm(new ArticleEditType(), $article);
-
+ 
     $request = $this->getRequest();
-
+ 
     if ($request->getMethod() == 'POST') {
       $form->bind($request);
-
+ 
       if ($form->isValid()) {
         // On enregistre l'article
         $em = $this->getDoctrine()->getManager();
         $em->persist($article);
         $em->flush();
-
+ 
         // On définit un message flash
         $this->get('session')->getFlashBag()->add('info', 'Article bien modifié');
-
+ 
         return $this->redirect($this->generateUrl('sdzblog_voir', array('id' => $article->getId())));
       }
     }
-
+ 
     return $this->render('SdzBlogBundle:Blog:modifier.html.twig', array(
       'form'    => $form->createView(),
       'article' => $article
     ));
   }
  
-  public function supprimerAction($id)
+  public function supprimerAction(Article $article)
   {
-    // On récupère l'EntityManager
-    $em = $this->getDoctrine()
-               ->getEntityManager();
+    // On crée un formulaire vide, qui ne contiendra que le champ CSRF
+    // Cela permet de protéger la suppression d'article contre cette faille
+    $form = $this->createFormBuilder()->getForm();
  
-    // On récupère l'entité correspondant à l'id $id
-    $article = $em->getRepository('SdzBlogBundle:Article')
-                  ->find($id);
-     
-    // Si l'article n'existe pas, on affiche une erreur 404
-    if ($article == null) {
-      throw $this->createNotFoundException('Article[id='.$id.'] inexistant');
-    }
+    $request = $this->getRequest();
+    if ($request->getMethod() == 'POST') {
+      $form->bind($request);
  
-    if ($this->get('request')->getMethod() == 'POST') {
-      // Si la requête est en POST, on supprimera l'article
-       
-      $this->get('session')->getFlashBag()->add('info', 'Article bien supprimé');
+      if ($form->isValid()) {
+        // On supprime l'article
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($article);
+        $em->flush();
  
-      // Puis on redirige vers l'accueil
-      return $this->redirect( $this->generateUrl('sdzblog_accueil') );
+        // On définit un message flash
+        $this->get('session')->getFlashBag()->add('info', 'Article bien supprimé');
+ 
+        // Puis on redirige vers l'accueil
+        return $this->redirect($this->generateUrl('sdzblog_accueil'));
+      }
     }
  
     // Si la requête est en GET, on affiche une page de confirmation avant de supprimer
     return $this->render('SdzBlogBundle:Blog:supprimer.html.twig', array(
-      'article' => $article
+      'article' => $article,
+      'form'    => $form->createView()
     ));
   }
  
--- src/Sdz/BlogBundle/Form/ImageType.php
+++ src/Sdz/BlogBundle/Form/ImageType.php
@@ -1,30 +1,29 @@
 <?php
 
 namespace Sdz\BlogBundle\Form;
-
+ 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-
+ 
 class ImageType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('url', 'text', array('required' => false))
-            ->add('alt', 'text', array('required' => false))
-        ;
-    }
-
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'Sdz\BlogBundle\Entity\Image'
-        ));
-    }
-
-    public function getName()
-    {
-        return 'sdz_blogbundle_imagetype';
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->add('file', 'file')
+    ;
+  }
+ 
+  public function setDefaultOptions(OptionsResolverInterface $resolver)
+  {
+    $resolver->setDefaults(array(
+      'data_class' => 'Sdz\BlogBundle\Entity\Image'
+    ));
+  }
+ 
+  public function getName()
+  {
+    return 'sdz_blogbundle_imagetype';
+  }
 }
