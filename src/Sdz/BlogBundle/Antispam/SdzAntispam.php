141d319058dfde6e2a2484026c1750561fe51a9d
adelinebechade <adelinebechade@gmail.com>
2013-11-06 15:24:30 +0100

    Début partie 4.4

--- src/Sdz/BlogBundle/Antispam/SdzAntispam.php
+++ src/Sdz/BlogBundle/Antispam/SdzAntispam.php
@@ -3,8 +3,26 @@
  
 namespace Sdz\BlogBundle\Antispam;
  
-class SdzAntispam
+class SdzAntispam extends \Twig_Extension
 {
+  /*
+   * Twig va exécuter cette méthode pour savoir quelle(s) fonction(s) ajoute notre service
+   */
+  public function getFunctions()
+  {
+    return array(
+      'checkIfSpam' => new \Twig_Function_Method($this, 'isSpam')
+    );
+  }
+ 
+  /*
+   * La méthode getName() identifie votre extension Twig, elle est obligatoire
+   */
+  public function getName()
+  {
+    return 'SdzAntispam';
+  }
+
   protected $mailer;
   protected $locale;
   protected $nbForSpam;
